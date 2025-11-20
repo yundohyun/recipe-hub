f4493b29b2aa3a87e22d1f82878ba13fe5e21de2
yundohyun <160732611+yundohyun@users.noreply.github.com>
2025-11-20 20:21:47 +0900

    [#2]; feat: 데이터베이스 유틸 추가

---
+++ src/main/java/it/dohyun/recipe_hub/database/DatabaseUtil.java
@@ -0,0 +1,21 @@
+package it.dohyun.recipe_hub.database;
+
+import java.sql.*;
+
+public class DatabaseUtil {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/recipe_hub", "root", "dongyang");
+	}
+
+	public static void close(Connection con, PreparedStatement st) throws SQLException {
+		st.close();
+		con.close();
+	}
+
+	public static void close(Connection con, PreparedStatement st, ResultSet rs) throws SQLException {
+		rs.close();
+		st.close();
+		con.close();
+	}
+}
