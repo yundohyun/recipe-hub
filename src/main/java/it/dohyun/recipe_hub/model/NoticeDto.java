9efd96b7d55b2d170f9d2a00062b74e297f69803
yundohyun <160732611+yundohyun@users.noreply.github.com>
2025-11-23 21:26:43 +0900

    [#2]; feat: Notice DTO & DAO 추가

---
+++ src/main/java/it/dohyun/recipe_hub/model/NoticeDto.java
@@ -0,0 +1,51 @@
+package it.dohyun.recipe_hub.model;
+
+import java.time.LocalDateTime;
+
+public class NoticeDto {
+  private String id;
+  private String title;
+  private String content;
+  private LocalDateTime created;
+  private LocalDateTime updated;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+
+  public LocalDateTime getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(LocalDateTime updated) {
+    this.updated = updated;
+  }
+}
