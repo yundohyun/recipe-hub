26c36a458a71a5115fb4b4c94d8a30b38718ed1e
yundohyun <160732611+yundohyun@users.noreply.github.com>
2025-11-23 21:26:19 +0900

    [#2]; feat: RecipeIngredient DTO & DAO 추가

---
+++ src/main/java/it/dohyun/recipe_hub/model/RecipeIngredientDto.java
@@ -0,0 +1,60 @@
+package it.dohyun.recipe_hub.model;
+
+import java.time.LocalDateTime;
+
+public class RecipeIngredientDto {
+  private String id;
+  private String recipeId;
+  private String ingredient;
+  private String amount;
+  private LocalDateTime created;
+  private LocalDateTime updated;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getRecipeId() {
+    return recipeId;
+  }
+
+  public void setRecipeId(String recipeId) {
+    this.recipeId = recipeId;
+  }
+
+  public String getIngredient() {
+    return ingredient;
+  }
+
+  public void setIngredient(String ingredient) {
+    this.ingredient = ingredient;
+  }
+
+  public String getAmount() {
+    return amount;
+  }
+
+  public void setAmount(String amount) {
+    this.amount = amount;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+
+  public LocalDateTime getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(LocalDateTime updated) {
+    this.updated = updated;
+  }
+}
