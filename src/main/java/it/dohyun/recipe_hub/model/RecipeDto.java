2fb6e2581de058ddb6c6b00d8d31a3aa3a71f7bb
yundohyun <160732611+yundohyun@users.noreply.github.com>
2025-11-23 21:25:09 +0900

    [#2]; feat: Recipe DTO & DAO 추가

---
+++ src/main/java/it/dohyun/recipe_hub/model/RecipeDto.java
@@ -0,0 +1,78 @@
+package it.dohyun.recipe_hub.model;
+
+import java.time.LocalDateTime;
+
+public class RecipeDto {
+  private String id;
+  private String memberId;
+  private String title;
+  private Integer serve;
+  private Integer duration;
+  private Integer viewCount;
+  private LocalDateTime created;
+  private LocalDateTime updated;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getMemberId() {
+    return memberId;
+  }
+
+  public void setMemberId(String memberId) {
+    this.memberId = memberId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Integer getServe() {
+    return serve;
+  }
+
+  public void setServe(Integer serve) {
+    this.serve = serve;
+  }
+
+  public Integer getDuration() {
+    return duration;
+  }
+
+  public void setDuration(Integer duration) {
+    this.duration = duration;
+  }
+
+  public Integer getViewCount() {
+    return viewCount;
+  }
+
+  public void setViewCount(Integer viewCount) {
+    this.viewCount = viewCount;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+
+  public LocalDateTime getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(LocalDateTime updated) {
+    this.updated = updated;
+  }
+}
