6ed893e0528940bcc3ff5c3a435179d329f36516
yundohyun <160732611+yundohyun@users.noreply.github.com>
2025-11-23 21:26:35 +0900

    [#2]; feat: RecipeContentImage DTO & DAO 추가

---
+++ src/main/java/it/dohyun/recipe_hub/model/RecipeContentImageDto.java
@@ -0,0 +1,22 @@
+package it.dohyun.recipe_hub.model;
+
+public class RecipeContentImageDto {
+  private String recipeContentId;
+  private String imageId;
+
+  public String getRecipeContentId() {
+    return recipeContentId;
+  }
+
+  public void setRecipeContentId(String recipeContentId) {
+    this.recipeContentId = recipeContentId;
+  }
+
+  public String getImageId() {
+    return imageId;
+  }
+
+  public void setImageId(String imageId) {
+    this.imageId = imageId;
+  }
+}
